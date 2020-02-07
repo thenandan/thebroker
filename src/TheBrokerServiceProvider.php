cbf49f7cfc7afa9e0f5a9918603dff392105053b
Keshari Nandan <keshari.nandan2@gmail.com>
2020-02-07 12:46:17 +0530

    [Fixes] Removed dummy code copied from other repo

--- src/TheBrokerServiceProvider.php
+++ src/TheBrokerServiceProvider.php
@@ -16,53 +16,14 @@ class TheBrokerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        if ($this->app->runningInConsole()) {
-            $this->bootForConsole();
-        }
 
-        /*
-         * Register the migrations
-         */
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
     /**
-     * @throws BindingResolutionException
+     * 
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/thelogger.php', 'thelogger');
 
-        $this->registerMiddleware();
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        //
-    }
-
-    /**
-     * Console-specific booting.
-     *
-     * @return void
-     */
-    protected function bootForConsole()
-    {
-        //
-    }
-
-    /**
-     * @throws BindingResolutionException
-     */
-    private function registerMiddleware()
-    {
-        $kernel = $this->app->make(Kernel::class);
-        $kernel->pushMiddleware(TheRequestLogger::class);
     }
 }
