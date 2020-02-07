f29812c7bb79406a659955387641e3938b82bcb7
Keshari Nandan <keshari.nandan2@gmail.com>
2020-02-07 13:18:02 +0530

    [Fixes] WOrking on queue

--- src/TheBrokerServiceProvider.php
+++ src/TheBrokerServiceProvider.php
@@ -4,7 +4,10 @@ namespace TheNandan\TheBroker;
 
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Queue\Connectors\SqsConnector;
+use Illuminate\Queue\QueueManager;
 use Illuminate\Support\ServiceProvider;
+use TheNandan\TheBroker\Connectors\RabbitMQConnector;
 use TheNandan\TheLogger\Http\Middleware\TheRequestLogger;
 
 class TheBrokerServiceProvider extends ServiceProvider
@@ -13,17 +16,34 @@ class TheBrokerServiceProvider extends ServiceProvider
      * Perform post-registration booting of services.
      *
      * @return void
+     *
+     * @throws BindingResolutionException
      */
     public function boot()
+    {
+
+        $manager = $this->app->make('queue');
+        $this->registerRebbitMQConnector($manager);
+    }
+
+    /**
+     *
+     */
+    public function register()
     {
 
     }
 
     /**
-     * 
+     * Register the Amazon RabbitMQ queue connector.
+     *
+     * @param  QueueManager  $manager
+     * @return void
      */
-    public function register()
+    protected function registerRebbitMQConnector($manager)
     {
-
+//        $manager->addConnector('rabbitmq', function () {
+//            return new RabbitMQConnector;
+//        });
     }
 }
